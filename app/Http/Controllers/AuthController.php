f2c97cc2791ba214c1c8f0f7a29ea77dffdf638c
Brahim El Manti <wiesbaden.hessen.Deutsh@gmail.Com>
2022-11-19 13:38:53 +0100

    learn-laravel

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Controller;
+use Validator;
+use App\Models\User;
+class AuthController extends Controller
+{
+    public function construct(){
+        $this->middleware('auth:api',['except'=>['login','register']]);
+    }
+   public function register(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name'=>'required',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|string|confirmed|min:6'
+        ]);
+        if($validator->fails()){
+            return response()->json($validator->error()->toJson(),400);
+
+        }
+        $user =User::create(array_merge(
+            $validator->validated(),
+            ['password'=>bcrypt($request->password)]
+        ));
+        return response()->json([
+            'message'=>'User successfully registred',
+            'user'=>$user
+        ],201);
+   }
+   public function login(Request $request){
+    $validator = Validator::make($request->all(),[
+        'email'=>'required|email',
+        'password'=>'required|string|min:6'
+    ]);
+    if($validator->fails()){
+        return response()->json($validator->error()->toJson(),422);
+
+    }
+    if(!$token=auth()->attempt($validator->validated())){
+        return response()->json(['error'=>'Unauthorized'],401);
+    }
+    return $this->createNewToken($token);
+
+   }
+   public function createNewToken($token){
+        return response()->json([
+            'access_token'=>$token,
+            'token_type'=>'bearer',
+            'expires_in'=>auth()->factory()->getTTL()*60,
+            'user'=>auth()->user()
+        ]);
+   }
+   public function profile(){
+        return response()->json(auth()->user());
+   }
+   public function logout(){
+        auth()->logout();
+        return response()->json([
+            'message'=>'User  logged out'
+
+        ]);
+   }
+}
